f76acf960153165b50a64bad0f7998f4fb69df82
Adam Oken <oak@Adams-MacBook-Pro.local>
2024-07-24 00:24:58 -0700

    Show spinner for loading genres

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,70 +1,74 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import { checkImage } from "./GameCard";
-import  Adventure from "../assets/Adventure.png"
-import  Arcade from "../assets/Arcade.png"
-import  CardGame from "../assets/CardGame.png"
-import  Fighting from "../assets/Fighting.png"
-import  HackSlash from "../assets/HackSlash.png"
-import  Indie from "../assets/Indie.png"
-import  MOBA from "../assets/MOBA.png"
-import  Music from "../assets/Music.png"
-import  Pinball from "../assets/Pinball.png"
-import  Platform from "../assets/Platform.png"
-import  PointAndClick from "../assets/PointAndClick.png"
-import  Puzzle from "../assets/Puzzle.png"
-import  Quiz from "../assets/Quiz.png"
-import  Racing from "../assets/Racing.png"
-import  RolePlaying from "../assets/RolePlaying.png"
-import  RTS from "../assets/RTS.png"
-import  Shooter from "../assets/Shooter.png"
-import  Simulator from "../assets/Simulator.png"
-import  Sport from "../assets/Sport.png"
-import  Strategy from "../assets/Strategy.png"
-import  Tactical from "../assets/Tactical.png"
-import  TurnBased from "../assets/TurnBased.png"
-import  VisualNovel from "../assets/VisualNovel.png"
+import Adventure from "../assets/Adventure.png";
+import Arcade from "../assets/Arcade.png";
+import CardGame from "../assets/CardGame.png";
+import Fighting from "../assets/Fighting.png";
+import HackSlash from "../assets/HackSlash.png";
+import Indie from "../assets/Indie.png";
+import MOBA from "../assets/MOBA.png";
+import Music from "../assets/Music.png";
+import Pinball from "../assets/Pinball.png";
+import Platform from "../assets/Platform.png";
+import PointAndClick from "../assets/PointAndClick.png";
+import Puzzle from "../assets/Puzzle.png";
+import Quiz from "../assets/Quiz.png";
+import Racing from "../assets/Racing.png";
+import RolePlaying from "../assets/RolePlaying.png";
+import RTS from "../assets/RTS.png";
+import Shooter from "../assets/Shooter.png";
+import Simulator from "../assets/Simulator.png";
+import Sport from "../assets/Sport.png";
+import Strategy from "../assets/Strategy.png";
+import Tactical from "../assets/Tactical.png";
+import TurnBased from "../assets/TurnBased.png";
+import VisualNovel from "../assets/VisualNovel.png";
 
-
-const genreMap: { [key: string] : string } = {
-["Adventure"] : Adventure,
-["Arcade"] : Arcade,
-["Card & Board Game"] : CardGame,
-["Fighting"] : Fighting,
-["Hack and slash/Beat 'em up"] : HackSlash,
-["Indie"] : Indie,
-["MOBA"] : MOBA,
-["Music"] : Music,
-["Pinball"] : Pinball,
-["Platform"] : Platform,
-["Point-and-click"] : PointAndClick,
-["Puzzle"]: Puzzle,
-["Quiz/Trivia"]: Quiz,
-["Racing"]: Racing,
-["Role-playing (RPG)"]: RolePlaying,
-["Real Time Strategy (RTS)"]: RTS,
-["Shooter"]: Shooter,
-["Simulator"]:Simulator,
-["Sport"]: Sport,
-["Strategy"]: Strategy,
-["Tactical"]: Tactical, 
-["Turn-based strategy (TBS)"]: TurnBased, 
-["Visual Novel"]:VisualNovel
-}
+const genreMap: { [key: string]: string } = {
+  ["Adventure"]: Adventure,
+  ["Arcade"]: Arcade,
+  ["Card & Board Game"]: CardGame,
+  ["Fighting"]: Fighting,
+  ["Hack and slash/Beat 'em up"]: HackSlash,
+  ["Indie"]: Indie,
+  ["MOBA"]: MOBA,
+  ["Music"]: Music,
+  ["Pinball"]: Pinball,
+  ["Platform"]: Platform,
+  ["Point-and-click"]: PointAndClick,
+  ["Puzzle"]: Puzzle,
+  ["Quiz/Trivia"]: Quiz,
+  ["Racing"]: Racing,
+  ["Role-playing (RPG)"]: RolePlaying,
+  ["Real Time Strategy (RTS)"]: RTS,
+  ["Shooter"]: Shooter,
+  ["Simulator"]: Simulator,
+  ["Sport"]: Sport,
+  ["Strategy"]: Strategy,
+  ["Tactical"]: Tactical,
+  ["Turn-based strategy (TBS)"]: TurnBased,
+  ["Visual Novel"]: VisualNovel,
+};
 
 const GenreList = () => {
-  const { data } = useGenres();
-    
+  const { data, isLoading, error} = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
-              <Image boxSize='32px' borderRadius={8} src={genreMap[genre.name]}/>
-              <Text fontSize='lg'>{genre.name}</Text>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={genreMap[genre.name]}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
             </HStack>
-            
           </ListItem>
         ))}
       </List>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,36 +2,31 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-const useData = <T>(endpoint: string) =>  {
-  
-    const [data, setData] = useState<T[]>([])
-    const [error, setError] = useState('')
-    const [isLoading, setLoading] = useState(false)
-  
-    useEffect(() => {    
-        console.log("inside genre effect");
-        
-      const controller = new AbortController();
-  
-      setLoading(true);
-      apiClient.get<T[]>(endpoint, { signal: controller.signal})
-        .then((response) => {
-            console.log("setting data", response.data);
+  useEffect(() => {
+    const controller = new AbortController();
 
-          setData(response.data)
-          setLoading(false);
-        })
-      .catch((err) => {
-          if(err instanceof CanceledError) return;
-          setError(err.message)
-          setLoading(false)
+    setLoading(true);
+    apiClient
+      .get<T[]>(endpoint, { signal: controller.signal })
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
       })
-  
-      return () => controller.abort();
-    }, [])
-    
-    return {data, error, isLoading}; 
-  }
-  
-  export default useData;
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { data, error, isLoading };
+};
+
+export default useData;
